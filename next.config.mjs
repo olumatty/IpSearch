c549daf4adf8e73ee5855df82fa384336a9b11fd
Matthew Adebayo <olumatty@gmail.com>
2024-11-25 22:27:03 +0100

    final commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: "/(.*)",
+            headers: [
+              {
+                key: "Content-Security-Policy",
+                value: "upgrade-insecure-requests",
+              },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
