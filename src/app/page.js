f35525dac0b2b622566603f6a9315ad327365410
Matthew Adebayo <olumatty@gmail.com>
2024-11-25 15:31:49 +0100

    third commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { CiLocationOn } from "react-icons/ci"
 
 const page = () => {
+  const [ipAddress, setIpAddress] = useState("")
   return (
-    <div className="flex flex-col py-20 px-8 text-center">
-      <h1 className="font-bold text-xl">IP ADDRESS SEARCH</h1>
+    <div className="flex flex-col py-20 px-8 ">
+      <h1 className="font-bold text-xl text-center">IP ADDRESS SEARCH</h1>
       <div className="border w-full py-8 px-4 mt-4">
-        <div className="flex justify-between">
+        <div className="flex justify-between gap-4">
           <div className="relative"> 
-            <div className="absolute h-20 w-20"><CiLocationOn /></div>
-            <input type="text" className="w-[240px] h-10 p-4 pl-8 border" placeholder="Enter Ip address" />
+            <div className="absolute top-2 left-2"><CiLocationOn className="h-6 w-6"/></div>
+            <input type="text" className="w-[260px] h-10 p-4 pl-12 border outline-none rounded" placeholder="Enter Ip address" />
           </div>
-          <button type="submit">Search</button>
+          <button className="border w-full font-bold bg-blue-500 text-white rounded cursor-pointer" type="submit">Search</button>
+        </div>
+        {/* result */}
+        <div className="mt-4">
+          <p>ip : </p>
+          <p>type : </p>
+          <p>Continent_name : </p>
+          <p>Country_name : </p>
+          <p>Country_code : </p>
+          <p>Region_name : </p>
+          <p>Region_code : </p>
+          <p>Latitude : </p>
+          <p>Longitude : </p>
+          <p>Zip : </p>
         </div>
       </div>
     </div>
